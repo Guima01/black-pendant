252fd8380d2abb1e114cea38024514e292bd6dc1
Guima01 <guilhermemarques1@ice.ufjf.br>
2020-08-09 22:57:37 -0300

    initial window created

---
+++ main.py
@@ -0,0 +1,57 @@
+"""
+Platformer Game
+"""
+import arcade
+
+# Constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Black Pendant"
+
+SKULL_SCALING = 1
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self):
+
+        # Call the parent class and set up the window
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+        self.player_list = None
+
+        # Separate variable that holds the player sprite
+        self.player_sprite = None
+
+    def setup(self):
+        """ Set up the game here. Call this function to restart the game. """
+        self.player_list = arcade.SpriteList()
+
+        self.player_sprite = arcade.Sprite("imagens/Dark_Ruler.png", SKULL_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
+    def on_draw(self):
+        """ Render the screen. """
+
+        arcade.start_render()
+        # Code to draw the screen goes here
+
+        self.player_list.draw()
+
+
+def main():
+    """ Main method """
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
